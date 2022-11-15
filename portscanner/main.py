c30576803dc049df8d3c604ac88af4f4ecd69af6
ab <ab.mhd@pm.me>
2022-11-14 22:09:18 -0500

    Static socket scanner

---
+++ portscanner/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+# socket library used for connectivity
+
+import socket
+
+# socket object is created and defined.
+# AF_INET used to define the socket and by designating the type of address the socket will communicate with(IP4)
+# SOCK_STREAM used to define the protocol that the socket will use at the transport layer (TCP)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# defining static socket
+
+target_ip = "192.168.2.33"
+target_port = 8888
+
+
+# port scanner function declared
+def portscanner(port):
+    if sock.connect_ex((target_ip, target_port)):
+
+        print("Port", port, " is closed")
+    else:
+
+        print("Port", port, " is open")
+
+
+# function call
+portscanner(target_port)
